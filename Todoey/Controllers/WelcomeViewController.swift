d024b28aa26be0fb48fe6ff3aacfe9538355b92c
Helena <helena.niu.2017@gmail.com>
2019-01-17 01:44:32 -0500

    Combine list with Welcome,Register, and Log in page.

---
+++ Todoey/Controllers/WelcomeViewController.swift
@@ -0,0 +1,47 @@
+//
+//  RegeitrationViewController.swift
+//  Todoey
+//
+//  Created by Helena on 1/17/19.
+//  Copyright © 2019 HelenaNiu. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//            let destinationVC = segue.destination as! TodoListViewController
+//
+//            if let indexPath = tableView.indexPathForSelectedRow {
+//                destinationVC.selectedCategory = categories?[indexPath.row]
+//            }
+//        }
+    
+//    let destinationVC = segue.destination as! RegisterViewController
+        
+        
+    
+    }
+}
